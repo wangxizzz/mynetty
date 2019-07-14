7c88b06d8cd77707ebb391ac8a2583764c992918
wangxizzz <979675820@qq.com>
2019-07-14 13:23:48 +0800

    自定义Long型处理器

---
+++ src/main/java/myhandler/MyServerInitializer.java
@@ -0,0 +1,28 @@
+package myhandler;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+/**
+ * <Description>
+ *
+ * @author wangxi
+ */
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        // 需要自定义解码器，把字节变为Long类型，在传入到我们自己的业务handler中
+        pipeline.addLast(new MyByteToLongDecoder());
+        pipeline.addLast(new MyLongToByteEncoder());
+        pipeline.addLast(new MyServerHandler());
+    }
+}
+
