ecfb859d54f15fb2be9c2cb7b798c7c9a584b7dc
wangxizzz <979675820@qq.com>
2019-12-15 18:10:25 +0800

    修改日志级别为info

--- mynetty/src/main/java/test/day01/FastThreadLocalTest.java
+++ mynetty/src/main/java/test/day01/FastThreadLocalTest.java
@@ -1,11 +1,18 @@
 package test.day01;
 
+import io.netty.util.concurrent.FastThreadLocal;
+
 /**
  * @Author wangxi
  * @Time 2019/12/15 17:56
  */
 public class FastThreadLocalTest {
-    public static void main(String[] args) {
 
+    private static FastThreadLocal<String> threadLocal = new FastThreadLocal<>();
+
+    public static void main(String[] args) {
+        threadLocal.set("aaa");
+        System.out.println(threadLocal.get());
+        threadLocal.remove();
     }
 }
